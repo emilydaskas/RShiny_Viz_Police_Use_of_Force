49cb92de137d8feece3ebb0c538e1583bd17642e
kaitlynabdo <45447032+kaitlynabdo@users.noreply.github.com>
2020-11-16 11:28:48 -0800

    Update Module_2_Emily_Kaitlyn.R

--- Module_2_Emily_Kaitlyn.R
+++ Module_2_Emily_Kaitlyn.R
@@ -35,6 +35,8 @@ uof_arrests.long.summary$percent_incidents <- uof_arrests.long.summary$total_arr
 
 uof_odds <- subset(uof, select = c(odds_ratio_pop, odds_ratio_arrests, coverage_city, county))
 
+uof_subjects_omit <- na.omit(uof_subjects)
+uof_arrests_omit <- na.omit(uof_arrests)
 
 ui = fluidPage(
   titlePanel("Comparing New Jersey Counties: Use of Force and Arrests by Race", window = "Group Module 2"),
@@ -43,6 +45,8 @@ ui = fluidPage(
       selectInput('selectCounty1', 'County 1', choices = sort(c(unique(uof$county))), selected = "Atlantic"),
       selectInput('selectCounty2', 'County 2', choices = sort(c(unique(uof$county))), selected= "Bergen"),          
       radioButtons('ethnType', 'Select View', choices = c("Use of Force Subjects", "Arrests")),
+      uiOutput('percentStats'),
+      uiOutput("space"),
       textOutput("fact")
     ),
     mainPanel(
@@ -82,7 +86,7 @@ server <- function(input,output) {
       filter(county == input$selectCounty1 || county == input$selectCounty2)
   })
   
-
+  
   output$percentPlot <- renderPlot({
     if (is.null(input$ethnType)){return ()}
     
@@ -133,20 +137,51 @@ server <- function(input,output) {
         theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1), title = element_text(size = 14, face = "bold"), axis.title = element_blank(), legend.text = element_text(size = 10)) 
     }
     
-     else if (input$ethnType == "Use of Force Subjects") {
+    else if (input$ethnType == "Use of Force Subjects") {
       ggplot(filtered_odds_2(), aes(x = coverage_city, y = odds_ratio_pop, group = county, fill = county)) + geom_bar(stat = "identity")+
-      #coord_flip() +
-      scale_fill_manual(values = c("#654F6F", "#A8C69F")) + 
-      labs(fill = "County", title = paste0("Odds Ratio of Use of Force Against a Black Person \nvs a White Person in ", input$selectCounty1, " and ", input$selectCounty2, " Counties"),
-           caption = "Towns with missing data are not represented in this graph.") +
-      theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1), title = element_text(size = 14, face = "bold"), axis.title = element_blank(), legend.text = element_text(size = 10))     }
+        #coord_flip() +
+        scale_fill_manual(values = c("#654F6F", "#A8C69F")) + 
+        labs(fill = "County", title = paste0("Odds Ratio of Use of Force Against a Black Person \nvs a White Person in ", input$selectCounty1, " and ", input$selectCounty2, " Counties"),
+             caption = "Towns with missing data are not represented in this graph.") +
+        theme(axis.text.x = element_text(angle = 70, vjust = 1, hjust=1), title = element_text(size = 14, face = "bold"), axis.title = element_blank(), legend.text = element_text(size = 10))     }
   }, width = 700)#, height = 650)
   
   output$fact = renderText({
     paste("Disclaimer: Ethnicities marked on forms were up to the discretion of the officers filling out incident forms. In addition to this, some county populations may be under-represented due to missing data or forms lacking options (such as Hispanic arrests).")
   })
   
+  output$space <- renderUI({
+    HTML(paste0("</br>"))
+  })
+  
+  output$percentStats <- renderUI({
+    if(input$ethnType == "Arrests") {
+      tArrW <- summary(aov(white_arrests_pct_2012_2016~factor(county), data=uof_arrests_omit))
+      tArrB <- summary(aov(black_arrests_pct_2012_2016~factor(county), data=uof_arrests_omit))
+      tArrAPI <- summary(aov(api_arrests_pct_2012_2016~factor(county), data=uof_arrests_omit))
+      
+      HTML(paste("An ANOVA analysis was performed between the arrests of different ethnic groups and county. The F value is for white suspects is  ",
+                 format(tArrW[[1]][[1,4]],digits=4), ".",
+                 "The F value is for black suspects is  ", format(tArrB[[1]][[1,4]],digits=4),
+                 ".", "The F value is for API suspects is  ", format(tArrAPI[[1]][[1,4]],digits=4), ".", "</br>"))
+    }
+    else if (input$ethnType == "Use of Force Subjects") {
+      tForceW <- summary(aov(white_pct_subjects~factor(county), data=uof_subjects_omit))
+      tForceB <- summary(aov(black_pct_subjects~factor(county), data=uof_subjects_omit))
+      tForceH <- summary(aov(hispanic_pct_subjects~factor(county), data=uof_subjects_omit))
+      tForceAPI <- summary(aov(asian_pacific_islander_pct_subjects~factor(county), data=uof_subjects_omit))
+      
+      HTML(paste("An ANOVA analysis was performed between the use of force on different ethnic groups and county. The F value is for white suspects is  ",
+                 format(tForceW[[1]][[1,4]],digits=3), ".",
+                 "The F value is for black suspects is  ", format(tForceB[[1]][[1,4]],digits=3), ".", 
+                 "The F value is for API suspects is  ", format(tForceAPI[[1]][[1,4]],digits=3), ".",
+                 "The F value is for hispanic suspects is  ", format(tForceB[[1]][[1,4]], digits=3), "."))
+      
+    }
+  })
+  
 }
-    
+
 
 shinyApp(ui, server)
+
