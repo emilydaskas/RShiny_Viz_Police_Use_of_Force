95e6ec7a2b6e10942fe681f70a17a4df4feeac93
Emily Daskas <daska102@mail.chapman.edu>
2020-11-15 22:46:13 -0800

    fixing na values

--- Module_2_Emily_Kaitlyn.R
+++ Module_2_Emily_Kaitlyn.R
@@ -1,8 +1,11 @@
+
+
 library(shiny)
 library(ggplot2)
 library(dplyr)
 library(reshape2)
 library(rsconnect)
+library(tidyverse)
 
 uof <- read.csv("UOF_BY_DEPARTMENTS.csv")
 
@@ -10,8 +13,11 @@ uof <- read.csv("UOF_BY_DEPARTMENTS.csv")
 uof_subjects <- uof %>% 
   select(county, white_pct_subjects, hispanic_pct_subjects, black_pct_subjects, 
          asian_pacific_islander_pct_subjects, total_incidents_2012_2016)
+
+
 uof_subjects.long <- melt(uof_subjects, id = c("county", "total_incidents_2012_2016"))
 uof_subjects.long <- melt(uof_subjects, id = c("county", "total_incidents_2012_2016"))
+uof_subjects.long <-  uof_subjects.long %>% drop_na(total_incidents_2012_2016)
 uof_subjects.long$incident_count <- uof_subjects.long$total_incidents_2012_2016 * uof_subjects.long$value / 100
 uof_subjects.long.summary <- uof_subjects.long %>% group_by(county, variable) %>% 
   summarize(total_incidents_race_county = sum(incident_count), total_incidents_county = sum(total_incidents_2012_2016))
@@ -22,6 +28,7 @@ uof_arrests <- uof %>%
   select(county, white_arrests_pct_2012_2016, black_arrests_pct_2012_2016, 
          api_arrests_pct_2012_2016, total_arrests_2012_2016)
 uof_arrests.long <- melt(uof_arrests, id = c("county", "total_arrests_2012_2016"))
+uof_arrests.long <-  uof_arrests.long %>% drop_na(total_arrests_2012_2016)
 uof_arrests.long$incident_count <- uof_arrests.long$total_arrests_2012_2016 * uof_arrests.long$value / 100
 uof_arrests.long.summary <- uof_arrests.long %>% group_by(county, variable) %>% 
   summarize(total_arrests_race_county = sum(incident_count), total_incidents_county = sum(total_arrests_2012_2016))
@@ -50,7 +57,12 @@ server <- function(input,output) {
   
   filtered_odds_2 <- reactive({
     uof_odds %>%
-      filter(county == input$selectCounty1 | county == input$selectCounty2)
+      filter(county == input$selectCounty1 | county == input$selectCounty2) %>% drop_na(odds_ratio_pop)
+  })
+  
+  filtered_odds_3 <- reactive({
+    uof_odds %>%
+      filter(county == input$selectCounty1 | county == input$selectCounty2) %>% drop_na(odds_ratio_arrests)
   })
   
   filtered <- reactive({
@@ -59,7 +71,7 @@ server <- function(input,output) {
     }
     
     uof_subjects.long.summary %>%
-      filter(county == input$selectCounty1 || county == input$selectCounty2)
+      filter(county == input$selectCounty1 || county == input$selectCounty2) 
   })
   
   filteredArrests <- reactive({
@@ -94,7 +106,7 @@ server <- function(input,output) {
       ggplot(filteredArrests(), aes(x= county, y= percent_incidents, group = variable, fill = variable)) + 
         geom_bar(position = "stack", stat = "identity", alpha = .8) + 
         scale_fill_manual(values = c( "yellow4", "darkblue", "maroon"), labels = c("White", "Black", "Asian/Pacifc Islander")) +
-        labs(fill = "Race", title = "% Arrests of a Race vs Total Arrests") +
+        labs(fill = "Race", title = "Percent of Arrests of Different Races") +
         theme(title = element_text(size = 14, face = "bold"), axis.title = element_blank(), axis.text = element_text(size = 15), legend.text = element_text(size = 10))    + 
         coord_flip() 
     }
@@ -103,7 +115,7 @@ server <- function(input,output) {
       ggplot(filtered(), aes(x= county, y= percent_incidents, group = variable, fill = variable)) + 
         geom_bar(position = "stack", stat = "identity", alpha = .8) + 
         scale_fill_manual(values = c( "yellow4", "purple", "darkblue", "maroon"), labels = c("White", "Hispanic", "Black", "Asian/Pacifc Islander")) +
-        labs(fill = "Race", title = "% Use of Force of a Race vs Total Use of Force ") +
+        labs(fill = "Race", title = "Percent of Races Who Were Victims of Use of Force ") +
         theme(title = element_text(size = 14, face = "bold"), axis.title = element_blank(), 
               axis.text = element_text(size = 15), legend.text = element_text(size = 10))   + 
         coord_flip()
@@ -115,26 +127,22 @@ server <- function(input,output) {
     
     if(input$ethnType == "Arrests") {
       
-      ggplot(filtered_odds_2(), aes(x = coverage_city, y = odds_ratio_arrests, group = county, fill = county)) + geom_bar(stat = "identity")+
+      ggplot(filtered_odds_3(), aes(x = coverage_city, y = odds_ratio_arrests, group = county, fill = county)) + geom_bar(stat = "identity")+
         coord_flip() +
         scale_fill_manual(values = c("#654F6F", "#A8C69F")) + 
-        labs(fill = "County", title = "Odds of arrest of a black person \nvs a white person in different counties") +
+        labs(fill = "County", title = paste0("Odds Ratio of arrest of a black person \nvs a white person in ", input$selectCounty1, " and ", input$selectCounty2, " Counties"),
+             caption = "Towns with missing data are not represented in this graph.") +
         theme(title = element_text(size = 14, face = "bold"), axis.title = element_blank(), legend.text = element_text(size = 10))
     }
-  
      else if (input$ethnType == "Use of Force Subjects") {
       ggplot(filtered_odds_2(), aes(x = coverage_city, y = odds_ratio_pop, group = county, fill = county)) + geom_bar(stat = "identity")+
       coord_flip() +
       scale_fill_manual(values = c("#654F6F", "#A8C69F")) + 
-      labs(fill = "County", title = "Odds of a Use of Force Against a black person \nvs a white person in different counties") +
-      theme(title = element_text(size = 14, face = "bold"), axis.title = element_blank(), legend.text = element_text(size = 10))
-    
-    }
+      labs(fill = "County", title = paste0("Odds Ratio of Use of Force Against a Black Person \nvs a White Person in ", input$selectCounty1, " and ", input$selectCounty2, " Counties"),
+           caption = "Towns with missing data are not represented in this graph.") +
+      theme(title = element_text(size = 14, face = "bold"), axis.title = element_blank(), legend.text = element_text(size = 10))     }
   }, height = 700)
 }
     
 
 shinyApp(ui, server)
-
-
-
